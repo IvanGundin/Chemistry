cf972551ee120e5efacfd2f5ae7f69f31336c61c
Иван Гундин <gia691992men@yandex.ru>
2022-11-09 18:43:37 +0300

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 window.addEventListener('scroll', e => {
 	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
 })
+
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 ScrollSmoother.create({
 	wrapper: '.wrapper',
 	content: '.content'
 })
+
+document.querySelector('.sidebar').onclick = function() {
+    document.querySelector('.side-menu').classList.toggle('hidden');
+    var el = document.querySelector('.layers__title');
+    el.style.display !== 'none' ? el.style.display = 'none' : el.style.display = 'initial';
+}
